10adb835d6cbde68994763cb242bf448ff29fede
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-10 17:21:54 +0530

    Time: 17 ms (73.08%), Space: 11 MB (33.54%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,12 +5,13 @@ public:
         unordered_map<int, int> mpp;
         
         for(int i=0; i<nums.size(); i++) {
-            if(mpp.find(target-nums[i])!=mpp.end()) {
+            if(mpp.find(target-nums[i]) != mpp.end()) {
                 ans.push_back(mpp[target-nums[i]]);
                 ans.push_back(i);
+                return ans;
             }
             mpp[nums[i]] = i;
-        }
+        }      
         return ans;
     }
 };
