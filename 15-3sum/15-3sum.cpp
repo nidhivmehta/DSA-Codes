5cb84101bd1e773de93e4c199ead0ab1eb5742b2
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-14 21:29:44 +0530

    Time: 145 ms (54.33%), Space: 23.5 MB (33.96%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,36 +1,32 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res; 
-        sort(nums.begin(), nums.end()); 
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
         
-        for (int i=0; i<(int)(nums.size())-2; i++) {
-            
-            if (i==0 || (i>0 && nums[i] != nums[i-1])) {
-                
-                int low=i+1, high=(int)(nums.size())-1, sum=0-nums[i];
-                
+        for(int i=0; i<nums.size()-2; i++) {
+            if(i==0 || i>0 and nums[i]!=nums[i-1]) {
+                int low = i+1;
+                int high = nums.size()-1;
+                int sum = 0-nums[i];
                 while(low<high) {
-                    if (nums[low]+nums[high] == sum) {
-                        
-                        vector<int> temp; 
-                        temp.push_back(nums[i]); 
-                        temp.push_back(nums[low]); 
-                        temp.push_back(nums[high]); 
+                    if(nums[low]+nums[high] == sum) {
+                        vector<int> temp;
+                        temp.push_back(nums[i]);
+                        temp.push_back(nums[low]);
+                        temp.push_back(nums[high]);
                         res.push_back(temp);
                         
-                        while (low<high && nums[low]==nums[low+1]) 
+                        while(low<high && nums[low]==nums[low+1])
                             low++;
-                        while (low<high && nums[high] == nums[high-1]) 
+                        while(low<high && nums[high]==nums[high-1])
                             high--;
-                        
-                        low++; high--;
-                    } 
-                    else if(nums[low]+nums[high]<sum) 
+                    }
+                    if(nums[low]+nums[high]<sum)
                         low++;
-                    else 
+                    else
                         high--;
-               }
+                }
             }
         }
         return res;
