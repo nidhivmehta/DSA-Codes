e3ac73b39bfde3fb9717a54f5c49d3945349dbfe
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-14 17:02:23 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (43.77%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -3,23 +3,21 @@ public:
     double myPow(double x, int n) {
         double ans = 1.0;
         long long nn = n;
+        if(nn<0)
+            nn = -1*nn;
         
-        if (nn < 0) 
-            nn = -1 * nn;
-        
-        while (nn) {
-            if (nn % 2) {
-                ans = ans * x;
-                nn = nn - 1;
-        } 
-        else {
-            x = x * x;
-            nn = nn / 2;
+        while(nn) {
+            if(nn%2 == 1) {
+                ans = ans*x;
+                nn = nn-1;
+            }
+            else {
+                x = x*x;
+                nn = nn/2;
+            }
         }
-      }
-        
-      if (n < 0) 
-        ans = (double)(1.0) / (double)(ans);
-      return ans;
-}
+        if(n<0)
+            ans = (double)1.0/(double)(ans);
+        return ans;
+    }
 };
