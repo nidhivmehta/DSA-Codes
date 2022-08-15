8d7c5be066be41c969ef43ed92982abca93cf6f9
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-15 18:29:20 +0530

    Time: 7 ms (53.86%), Space: 11.1 MB (75.18%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -4,8 +4,9 @@ public:
         int low = 0;
         int high = nums.size()-1;
         
-        while(low <= high) {
-            int mid = low+(high-low)/2;
+        while(low<=high) {
+            int mid = low + (high-low)/2;
+            
             if(nums[mid] == target)
                 return mid;
             
@@ -15,14 +16,13 @@ public:
                 else
                     low = mid+1;
             }
-            
             else {
-                if(target>=nums[mid] && target<=nums[high])
+                if(target<=nums[high] && target>=nums[mid])
                     low = mid+1;
                 else
                     high = mid-1;
             }
-        } 
+        }
         return -1;
     }
 };
