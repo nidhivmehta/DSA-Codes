8577d8b167ddd8634481eb6dc9bbe3bf805a869a
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-15 16:51:14 +0530

    Time: 33 ms (16.87%), Space: 10.3 MB (25.64%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &dp) {
+    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
         if(j<0 || j>=matrix[0].size())
             return 1e8;
+        
         if(i==0)
             return matrix[0][j];
         
-        if(dp[i][j] != -1)
+        if(dp[i][j]!=-1)
             return dp[i][j];
         
-        int u = matrix[i][j] + solve(i-1, j, matrix, dp);
-        int ud = matrix[i][j] + solve(i-1, j-1, matrix, dp);
-        int ld = matrix[i][j] + solve(i-1, j+1, matrix, dp);
+        int up = matrix[i][j] + solve(i-1, j, matrix, dp);
+        int ld = matrix[i][j] + solve(i-1, j-1, matrix, dp);
+        int rd = matrix[i][j] + solve(i-1, j+1, matrix, dp);
         
-        return dp[i][j] = min(u, min(ud, ld));
+        return dp[i][j] = min(up, min(ld, rd));
     }
     
     int minFallingPathSum(vector<vector<int>>& matrix) {
@@ -24,7 +25,6 @@ public:
         int mini = 1e8;
         for(int j=0; j<m; j++) 
             mini = min(mini, solve(n-1, j, matrix, dp));
-        
         return mini;
     }
 };
