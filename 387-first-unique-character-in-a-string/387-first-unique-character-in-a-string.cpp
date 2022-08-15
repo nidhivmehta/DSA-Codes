0e50c2010e81bc674a36473ed7487b6f41040dff
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-15 16:13:13 +0530

    Time: 45 ms (65.13%), Space: 10.6 MB (71.24%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -4,12 +4,13 @@ public:
         int n = s.size();
         vector<int> frequency(26, 0);
         
-        for(int i=0; s[i]!='\0'; i++)
-            frequency[s[i] - 'a'] += 1;
+        for(int i=0; s[i]!='\0'; i++) {
+            frequency[s[i]-'a'] += 1;
+        }
         
-        for(int i=0; s[i]!='\0'; i++)
-            if(frequency[s[i] - 'a'] == 1)
+        for(int i=0; s[i]!='\0'; i++) 
+            if(frequency[s[i]-'a'] == 1)
                 return i;
-            return -1;
+        return -1;
     }
 };
