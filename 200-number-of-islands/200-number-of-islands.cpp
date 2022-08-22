2c7189e51b2e654121fd95504c65a022576a858a
Nidhi Mehta <107789227+nidhivmehta@users.noreply.github.com>
2022-08-22 20:00:43 +0530

    Time: 46 ms (71.31%), Space: 12.3 MB (83.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
-    void solve(int x, int y, int r, int c, vector<vector<char>>& matrix) {
-        if(x<0 || x>=r || y<0 || y>=c || matrix[x][y]!='1')
+    void solve(int x, int y, int r, int c, vector<vector<char>>& grid) {
+        if(x<0 || x>=r || y<0 || y>=c || grid[x][y] != '1')
             return;
         
-        matrix[x][y] = '2';
+        grid[x][y] = '2';
         
-        solve(x-1, y, r, c, matrix);
-        solve(x, y+1, r, c, matrix);
-        solve(x+1, y, r, c, matrix);
-        solve(x, y-1, r, c, matrix);
+        solve(x-1, y, r, c, grid);
+        solve(x+1, y, r, c, grid);
+        solve(x, y-1, r, c, grid);
+        solve(x, y+1, r, c, grid);
     }
     
     int numIslands(vector<vector<char>>& grid) {
-        int totalIslands = 0;
+        int totalIsland = 0;
         int rows = grid.size();
+        int cols = grid[0].size();
         
         if(rows == 0)
             return 0;
         
-        int cols = grid[0].size();
-        
-        for(int i=0; i<rows; i++)
+        for(int i=0; i<rows; i++) 
             for(int j=0; j<cols; j++)
                 if(grid[i][j] == '1') {
                     solve(i, j, rows, cols, grid);
-                    totalIslands++;
-                }
-        return totalIslands;
-                    
-        
+                    totalIsland++;
+                    }
+        return totalIsland;     
     }
 };
